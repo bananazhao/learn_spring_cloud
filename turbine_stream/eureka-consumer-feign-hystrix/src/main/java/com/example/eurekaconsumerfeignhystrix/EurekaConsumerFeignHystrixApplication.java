22edfce73d8e4aa49b766e41f614a9eb764c57ee
小小笨鐘 <1015765948@qq.com>
2020-06-22 17:37:22 +0800

    Hystrix 监控数据聚合 Turbine

--- turbine_stream/eureka-consumer-feign-hystrix/src/main/java/com/example/eurekaconsumerfeignhystrix/EurekaConsumerFeignHystrixApplication.java
+++ turbine_stream/eureka-consumer-feign-hystrix/src/main/java/com/example/eurekaconsumerfeignhystrix/EurekaConsumerFeignHystrixApplication.java
@@ -2,10 +2,12 @@ package com.example.eurekaconsumerfeignhystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
 public class EurekaConsumerFeignHystrixApplication {
 
     public static void main(String[] args) {
