1ed949ee2624279176a9f12b6ca3e70cc31c8f30
小小笨鐘 <1015765948@qq.com>
2020-06-22 17:09:46 +0800

    Hystrix 监控数据聚合 Turbine

---
+++ turbine_stream/eureka-consumer-feign-hystrix/src/main/java/com/example/eurekaconsumerfeignhystrix/EurekaConsumerFeignHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaconsumerfeignhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConsumerFeignHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerFeignHystrixApplication.class, args);
+    }
+
+}
